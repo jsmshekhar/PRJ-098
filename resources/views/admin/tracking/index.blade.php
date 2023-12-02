543d61d555557df325cb2117bd2a6245d92f2e88
jsmshekhar <jsmshekhar62@gmail.com>
2023-12-03 01:08:32 +0530

    live tracking and 3 page design

--- resources/views/admin/tracking/index.blade.php
+++ resources/views/admin/tracking/index.blade.php
@@ -6,11 +6,111 @@
     </style>
 @endsection
 @section('content')
-    <div class="row card_dash">
-        <div class="col-12">
-            <h3>Live Tracking</h3>
-        </div>
-    </div><!-- end row-->
+<div class="container-fluid">
+                    <div class="row">
+                        <div class="col-md-4">
+                            <div class="card">
+                                <div class="card-body ">
+                                    <div class="detail_cnt live_track">
+                                        <form class="app-search d-none d-lg-block">
+                                            <div class="position-relative">
+                                                <img src="http://localhost/PRJ-098/public/assets/images/icons/Search.svg" alt="" class="search_inpot">
+                                                <input type="text" class="form-control w-100" placeholder="Filter Employees">
+                                            </div>
+                                        </form>
+                                        <ul>
+                                            <li>
+                                                <div class="form-check my-2">
+                                                    <input class="form-check-input" type="checkbox" name="is_send_charge" id="is_send_charge" >
+                                                    <label class="form-check-label pt-1 px-2 w-100" for="formCheck1">
+                                                        Aman Verma <span class="text-success m-0 float-end">Active</span>
+                                                    </label>
+                                                </div>
+                                            </li>
+                                            <li>
+                                                <div class="form-check my-2">
+                                                    <input class="form-check-input" type="checkbox" name="is_send_charge" id="is_send_charge" >
+                                                    <label class="form-check-label pt-1 px-2 w-100" for="formCheck1">
+                                                        Aman Verma <span class="text-success m-0 float-end">Active</span>
+                                                    </label>
+                                                </div>
+                                            </li>
+                                            <li>
+                                                <div class="form-check my-2">
+                                                    <input class="form-check-input" type="checkbox" name="is_send_charge" id="is_send_charge" >
+                                                    <label class="form-check-label pt-1 px-2 w-100" for="formCheck1">
+                                                        Aman Verma <span class="text-success m-0 float-end">Active</span>
+                                                    </label>
+                                                </div>
+                                            </li>
+                                            <li>
+                                                <div class="form-check my-2">
+                                                    <input class="form-check-input" type="checkbox" name="is_send_charge" id="is_send_charge" >
+                                                    <label class="form-check-label pt-1 px-2 w-100" for="formCheck1">
+                                                        Aman Verma <span class="text-success m-0 float-end">Active</span>
+                                                    </label>
+                                                </div>
+                                            </li>
+                                            <li>
+                                                <div class="form-check my-2">
+                                                    <input class="form-check-input" type="checkbox" name="is_send_charge" id="is_send_charge" >
+                                                    <label class="form-check-label pt-1 px-2 w-100" for="formCheck1">
+                                                        Aman Verma <span class="text-success m-0 float-end">Active</span>
+                                                    </label>
+                                                </div>
+                                            </li>
+                                            <li>
+                                                <div class="form-check my-2">
+                                                    <input class="form-check-input" type="checkbox" name="is_send_charge" id="is_send_charge" >
+                                                    <label class="form-check-label pt-1 px-2 w-100" for="formCheck1">
+                                                        Aman Verma <span class="text-success m-0 float-end">Active</span>
+                                                    </label>
+                                                </div>
+                                            </li>
+                                            <li>
+                                                <div class="form-check my-2">
+                                                    <input class="form-check-input" type="checkbox" name="is_send_charge" id="is_send_charge" >
+                                                    <label class="form-check-label pt-1 px-2 w-100" for="formCheck1">
+                                                        Aman Verma <span class="text-success m-0 float-end">Active</span>
+                                                    </label>
+                                                </div>
+                                            </li>
+                                            <li>
+                                                <div class="form-check my-2">
+                                                    <input class="form-check-input" type="checkbox" name="is_send_charge" id="is_send_charge" >
+                                                    <label class="form-check-label pt-1 px-2 w-100" for="formCheck1">
+                                                        Aman Verma <span class="text-success m-0 float-end">Active</span>
+                                                    </label>
+                                                </div>
+                                            </li>
+                                            <li>
+                                                <div class="form-check my-2">
+                                                    <input class="form-check-input" type="checkbox" name="is_send_charge" id="is_send_charge" >
+                                                    <label class="form-check-label pt-1 px-2 w-100" for="formCheck1">
+                                                        Aman Verma <span class="text-success m-0 float-end">Active</span>
+                                                    </label>
+                                                </div>
+                                            </li>
+
+                                        </ul>
+                                    </div>
+                                </div><!-- end card-body -->
+                            </div><!-- end card -->
+                        </div><!-- end col -->
+
+                        <div class="col-md-8">
+                        <div class="card">
+                                <div class="card-body personal-info-h">
+                                    <div class="detail_cnt">
+                                      
+                                    </div>
+                                </div><!-- end card-body -->
+                            </div><!-- end card -->
+                        </div><!-- end col -->
+                       
+                    </div> <!-- end row -->
+
+                </div>
 @endsection
 @section('js')
     <script></script>
--- resources/views/admin/wallet/index.blade.php
+++ resources/views/admin/wallet/index.blade.php
@@ -6,11 +6,186 @@
     </style>
 @endsection
 @section('content')
-    <div class="row card_dash">
+<div class="container-fluid">
+    <div class="row">
         <div class="col-12">
-            <h3>Wallet Management</h3>
-        </div>
-    </div><!-- end row-->
+            <div class="card">
+                <div class="card-body p-0">
+                    <div class="accordion" id="accordionExample">
+                        <div class="accordion-item">
+                            <h2 class="accordion-header" id="headingOne">
+                                <button class="accordion-button " type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                                    Advance Search
+                                </button>
+                                <div class="collaps_btns">
+                                    <button type="button" class="btn btn-outline-danger waves-effect waves-light" onclick="clearSearch('http://localhost/PRJ-098/admin/distributed-hubs');">Clear</button>
+                                    <button type="button" onclick="submitSearchForm();" class="btn btn-outline-success waves-effect waves-light">Search</button>
+                                                                    </div>
+                            </h2>
+                            <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                                <div class="accordion-body">
+                                    <form id="searchForm" method="get" action="http://localhost/PRJ-098/admin/distributed-hubs">
+                                        <input type="hidden" name="is_search" value="1">
+                                        <input type="hidden" name="per_page" id="perPageHidden">
+                                        <div class="row">
+                                            <div class="col-xl-3 col-md-6">
+                                                <div class="form-group mb-3">
+                                                    <label class="form-label">Customer Id</label>
+                                                    <input type="text" class="form-control" name="hub_id" value="">
+                                                </div>
+                                            </div>
+                                            <div class="col-xl-3 col-md-6">
+                                                <div class="form-group mb-3">
+                                                    <label class="form-label">Customer Name</label>
+                                                    <input type="text" class="form-control" name="city" value="">
+                                                </div>
+                                            </div>
+                                            
+                                            <div class="col-xl-3 col-md-6">
+                                                <div class="form-group mb-3">
+                                                    <label class="form-label">Available Balance</label>
+                                                    <input type="text" class="form-control" name="hub_capacity" value="">
+                                                </div>
+                                            </div>
+                                            <div class="col-xl-3 col-md-6">
+                                                <div class="form-group mb-3">
+                                                    <label class="form-label">Email</label>
+                                                    <input type="text" class="form-control" name="vehicle" value="">
+                                                </div>
+                                            </div>
+                                            <div class="col-xl-3 col-md-6">
+                                                <div class="form-group mb-3">
+                                                    <label class="form-label">Latest Transaction</label>
+                                                    <input type="text" class="form-control" name="hub_id" value="">
+                                                </div>
+                                            </div>                                            
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div><!-- end accordion -->
+                </div><!-- end card-body -->
+            </div><!-- end card -->
+        </div><!-- end col -->
+
+
+        <div class="col-12">
+            <div class="card">
+                <div class="card-body p-0">
+                    <div class="table-filter">
+                        <ul>
+                            <li>
+                                <a href="javascript:void(0);" class="btn btn-link" onclick="refreshPage();">
+                                    <img src="http://localhost/PRJ-098/public/assets/images/icons/refresh.svg" alt="">
+                                </a>
+                            </li>
+                            <li>
+                                <p>Total Record : <span>255</span></p>
+                            </li>
+                            <li>
+                                <p>Display up to :
+                                </p><div class="form-group">
+                                    <select class="form-control perPage" name="choices-single-no-sorting" id="perPageDropdown" onchange="perPage(this);">
+                            <option value="25">25</option>
+                            <option value="50">50</option>
+                            <option value="100">100</option>
+    </select>
+                                </div>
+                                <p></p>
+                            </li>
+                            <li>
+                                <button type="button" class="btn btn-success waves-effect waves-light">
+                                    <img src="http://localhost/PRJ-098/public/assets/images/icons/download.svg" alt="">
+                                    Export
+                                </button>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="table-rep-plugin">
+                                                <div class="table-responsive mb-0 fixed-solution" data-pattern="priority-columns">
+                            <div class="sticky-table-header">
+                                <table class="table">
+                                    <thead>
+                                        <tr>
+                                            <th>CUSTOMER ID</th>
+                                            <th>CUSTOMER NAME</th>
+                                            <th>AVAILABLE BALANCE</th>                                            
+                                            <th>EMAIL</th>
+                                            <th>LATEST TRANSACTION DATE</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td>RD001</td>
+                                            <td>Ankit Kumar</td>
+                                            <td>4000/-</td>
+                                            <td>Ankit@gmail.com</td>
+                                            <td>Refund Initiated</td>
+                                        </tr>
+                                        <tr>
+                                            <td>RD001</td>
+                                            <td>Ankit Kumar</td>
+                                            <td>4000/-</td>
+                                            <td>Ankit@gmail.com</td>
+                                            <td>Refund Initiated</td>
+                                        </tr>
+                                        <tr>
+                                            <td>RD001</td>
+                                            <td>Ankit Kumar</td>
+                                            <td>4000/-</td>
+                                            <td>Ankit@gmail.com</td>
+                                            <td>Refund Initiated</td>
+                                        </tr>
+                                        <tr>
+                                            <td>RD001</td>
+                                            <td>Ankit Kumar</td>
+                                            <td>4000/-</td>
+                                            <td>Ankit@gmail.com</td>
+                                            <td>Refund Initiated</td>
+                                        </tr>
+                                        <tr>
+                                            <td>RD001</td>
+                                            <td>Ankit Kumar</td>
+                                            <td>4000/-</td>
+                                            <td>Ankit@gmail.com</td>
+                                            <td>Refund Initiated</td>
+                                        </tr>
+                                        <tr>
+                                            <td>RD001</td>
+                                            <td>Ankit Kumar</td>
+                                            <td>4000/-</td>
+                                            <td>Ankit@gmail.com</td>
+                                            <td>Refund Initiated</td>
+                                        </tr>
+                                        <tr>
+                                            <td>RD001</td>
+                                            <td>Ankit Kumar</td>
+                                            <td>4000/-</td>
+                                            <td>Ankit@gmail.com</td>
+                                            <td>Refund Initiated</td>
+                                        </tr>
+                                        <tr>
+                                            <td>RD001</td>
+                                            <td>Ankit Kumar</td>
+                                            <td>4000/-</td>
+                                            <td>Ankit@gmail.com</td>
+                                            <td>Refund Initiated</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                            
+                        </div>
+                                            </div>
+
+                </div>
+            </div>
+            <!-- end card -->
+        </div> <!-- end col -->
+    </div> <!-- end row -->
+
+</div>
 @endsection
 @section('js')
     <script></script>
