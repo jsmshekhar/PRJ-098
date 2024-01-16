a9f0d084943c547f0d361df22ab30253b3eac778
Raj Kumar <raj.k@e2logy.com>
2024-01-16 17:36:53 +0530

    <<dashboar chart donut>>

--- resources/views/admin/home/dashboard.blade.php
+++ resources/views/admin/home/dashboard.blade.php
@@ -123,20 +123,20 @@
                             <div class="col-md-6">
                                 <div class="data_dash_grp">
                                     <div class="dash_grp_head">
-                                        <h4>Two Wheelers</h4>                    
+                                        <h4>Two Wheelers</h4>
                                     </div>
                                     <div class="dash_body_grp">
-                                        <div id="donut_chart" data-colors='["#2ab57d", "#5156be", "#fd625e", "#4ba6ef", "#ffbf53"]' class="apex-charts"  dir="ltr"></div>
+                                        <div id="donutChart2W"></div>
                                     </div>
                                 </div>
                             </div>
                             <div class="col-md-6">
                                 <div class="data_dash_grp">
                                     <div class="dash_grp_head">
-                                        <h4>Two Wheelers</h4>                    
+                                        <h4>Three Wheelers</h4>
                                     </div>
                                     <div class="dash_body_grp">
-                                        hello
+                                        <div id="donutChart3W"></div>
                                     </div>
                                 </div>
                             </div>
@@ -167,5 +167,90 @@
 
 @endsection
 @section('js')
-<script></script>
+<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+</script>
+<script type="text/javascript">
+    google.charts.load('current', {
+        callback: function() {
+            var data = google.visualization.arrayToDataTable([
+                ['Task', 'Two Wheeler'],
+                ['Mobilized Vehicle', <?php echo $evstatus2W->mobilized; ?>],
+                ['Immobilized Vehicle', <?php echo $evstatus2W->immobilized; ?>],
+                ['Functional Vehicle', <?php echo $evstatus2W->functional; ?>],
+                ['Non-Functional Vehicle', <?php echo $evstatus2W->non_functional; ?>],
+            ]);
+            var options = {
+                pieHole: 0.4,
+                pieSliceText: 'percentage',
+                theme: 'maximized'
+            };
+
+            var pieChart = new google.visualization.PieChart(document.getElementById('donutChart2W'));
+            pieChart.draw(data, options);
+
+            // Calculate and display total value inside pie hole
+            var totalValue = "<?php echo $evCount2W; ?>";
+
+            var totalLabel = document.createElement('div');
+            totalLabel.innerHTML = "Total EVs <br> <b style='text-align:center'>" + totalValue + "<b>";
+            totalLabel.style.position = 'absolute';
+            totalLabel.style.top = '60%';
+            totalLabel.style.left = '50%';
+            totalLabel.style.transform = 'translate(-50%, -50%)';
+            totalLabel.style.fontSize = '14px';
+
+            document.getElementById('donutChart2W').appendChild(totalLabel);
+        },
+        packages: ['corechart']
+    });
+</script>
+<script type="text/javascript">
+    google.charts.load('current', {
+        callback: function() {
+            var data = google.visualization.arrayToDataTable([
+                ['Task', 'Three Wheeler'],
+                ['Mobilized Vehicle', <?php echo isset($evstatus3W->mobilized) ? $evstatus3W->mobilized : 0; ?>],
+                ['Immobilized Vehicle', <?php echo isset($evstatus3W->immobilized) ? $evstatus3W->immobilized : 0; ?>],
+                ['Functional Vehicle', <?php echo isset($evstatus3W->functional) ? $evstatus3W->functional : 0; ?>],
+                ['Non-Functional Vehicle', <?php echo isset($evstatus3W->non_functional) ? $evstatus3W->non_functional : 0; ?>],
+            ]);
+
+            var data1 = google.visualization.arrayToDataTable([
+                ['Task', 'Three Wheeler'],
+                ['Mobilized Vehicle', 20],
+                ['Immobilized Vehicle', 9],
+                ['Functional Vehicle', 25],
+                ['Non-Functional Vehicle', 4],
+            ]);
+            var options = {
+                pieHole: 0.4,
+                pieSliceText: 'percentage',
+                theme: 'maximized',
+                colors: ['#4285F4', '#34A853', '#FBBC05', '#EA4335', '#0F9D58'] // Set colors for each section
+            };
+            var evCount3W = "<?php echo $evCount3W; ?>";
+            if (evCount3W > 0) {
+                var datas = data;
+            } else {
+                var datas = data1;
+            }
+            var pieChart = new google.visualization.PieChart(document.getElementById('donutChart3W'));
+            pieChart.draw(datas, options);
+
+            // Calculate and display total value inside pie hole
+            var totalValue = "<?php echo $evCount3W > 0 ? $evCount3W : 29; ?>"
+
+            var totalLabel = document.createElement('div');
+            totalLabel.innerHTML = "Total EVs <br> <b style='text-align:center'>" + totalValue + "<b>";
+            totalLabel.style.position = 'absolute';
+            totalLabel.style.top = '60%';
+            totalLabel.style.left = '50%';
+            totalLabel.style.transform = 'translate(-50%, -50%)';
+            totalLabel.style.fontSize = '12px';
+
+            document.getElementById('donutChart3W').appendChild(totalLabel);
+        },
+        packages: ['corechart']
+    });
+</script>
 @endsection
